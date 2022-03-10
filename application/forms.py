7745bf843a8c96587053e5b068d9cdd0ea50c0b7
Catman9000 <ellahiphotography@gmail.com>
2022-03-10 15:44:26 +0000

    created working website with C functionality in crud

--- application/forms.py
+++ application/forms.py
@@ -1,9 +1,28 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField, DateField, SubmitField
+from flask import Flask
+from application import app
+from flask import request
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import validators
 
-class AddToDo(FlaskForm):
-    title = StringField("Task Name")
-    desc = StringField("Task Description")
-    status = StringField("Status", choices=[('todo', 'todo'), ('done', 'done')])
-    proj_id = IntegerField("Project to Link")
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
+from wtforms.validators import InputRequired, Length, Regexp, NumberRange, DataRequired
+from datetime import date
+
+class AddProduct(FlaskForm):
+    product_name = StringField('Product name', validators=[DataRequired(message="This field cannot be left blank")])
+    product_cost = IntegerField("Cost", validators=[DataRequired(message="This field cannot be left blank")])
+    product_description = StringField("Product Description", validators=[DataRequired(message="This field cannot be left blank")])
     submit = SubmitField("Add Item")
+
+class UpdateProduct(FlaskForm):
+    product_name = StringField('Product name', validators=[DataRequired(message="This field cannot be left blank")])
+    product_cost = IntegerField("Cost", validators=[DataRequired(message="This field cannot be left blank")])
+    product_description = StringField("Product Description", validators=[DataRequired(message="This field cannot be left blank")])
+    submit = SubmitField("Update Item")
+
+class ChooseProduct(FlaskForm):
+    chosenProduct = SelectField('Product', choices=[])
+    submit = SubmitField("Choose Product")
+
--- application/models.py
+++ application/models.py
@@ -1,28 +1,42 @@
 from application import db
 from sqlalchemy.orm import relationship
+from wtforms import validators
 
 class user(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(75))
     user_email = db.Column(db.String(75))
     user_address = db.Column(db.String(75))
-    orders = db.relationship('Order', backref='person',
+    orders = db.relationship('Orders', backref='user',
                             lazy='dynamic')
+    def __repr__(self):
+        return f"{self.username}" 
 
 
 class Product(db.Model):
+    __tablename__ = 'Product'
     id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(150))
     product_description = db.Column(db.String(150))
-    product_cost = db.Column(db.Float(6,2))
+    product_cost = db.Column(db.Numeric(6,2))
+    # fk_product_type_id = db.Column(db.Integer, db.ForeignKey('Product_Type.id'))
+    def __repr__(self):
+        return f"{self.product_name}"    
 
+# class Product_Type(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     product_category = db.Column(db.String(150))
+#     def __repr__(self):
+#         return f"{self.product_category}"
 
-class Product_Type(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    product_category = db.Column(db.String(150))
-    products_id = db.Column(db.Integer, db.ForeignKey('Product.id'))
-
-class Order(db.Model):
+class Orders(db.Model):
+    __tablename__ = 'Orders'
     id = db.Column(db.Integer, primary_key=True)
     order_placed = db.Column(db.DATETIME)
-    order_total = db.Column(db.Float(7,2))
+    order_total = db.Column(db.Numeric(7,2))
+    orderer = db.Column(db.Integer, db.ForeignKey('user.id'))
+    product_ordered = db.Column(db.Integer, db.ForeignKey('Product.id'))
+    def __repr__(self):
+        return f"{self.order_placed}"
     
